8770a9ef16e1eb649d19a9ca58e32243156db31c
Faizan <faizansohail077@gmail.com>
2022-11-27 00:32:27 +0500

    completed typescript calculator

--- index.js
+++ index.js
@@ -1,15 +1,116 @@
 import inquirer from 'inquirer';
-inquirer
-    .prompt([{ name: 'new age2', type: 'number', 'message': 'whats your new age' }])
-    .then((answers) => {
-    console.log(answers, 'answers');
-    // Use user feedback for... whatever!!
-})
-    .catch((error) => {
-    if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+const Welcome = async () => {
+    let rainbow = chalkAnimation.karaoke(chalk.underline("Welcome To Faizan's Calculator"));
+    await Animation();
+    console.log(`_____________________
+    |  _________________  |
+    | | Faizan          0.| 
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|`);
+    rainbow.stop();
+    AskQuestion();
+};
+const Animation = () => {
+    return new Promise((res) => {
+        setTimeout(res, 1000);
+    });
+};
+const AskQuestion = async () => {
+    inquirer.prompt([{
+            name: "num1",
+            type: "number",
+            message: "Enter 1st Value",
+            validate: function (value) {
+                var pass = !isNaN(value);
+                if (pass) {
+                    return true;
+                }
+                return console.log(chalk.red('Please enter a valid number'));
+            },
+        },
+        {
+            name: "num2",
+            type: "number",
+            message: "Enter 2nd Value"
+        },
+        {
+            name: "operator",
+            type: "list",
+            choices: [{
+                    name: "Add",
+                    type: "number",
+                },
+                {
+                    name: "Substract",
+                    type: "number",
+                },
+                {
+                    name: "Divide",
+                    type: "number",
+                },
+                {
+                    name: "Multiply",
+                    type: "number",
+                },
+            ],
+            message: "Select Operator"
+        }
+    ]).then((res) => {
+        calulator(res);
+        setTimeout(() => {
+            PlayAgain();
+        }, 1000);
+    }).catch((err) => {
+        console.log(err, 'ererer');
+    });
+};
+const calulator = (answer) => {
+    if (answer.operator === "Add") {
+        return console.log(chalk.cyan(`${answer.num1} + ${answer.num2} =  ${answer.num1 + answer.num2}`));
+    }
+    else if (answer.operator === "Substract") {
+        return console.log(chalk.cyan(`${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2}`));
+    }
+    else if (answer.operator === "Divide") {
+        return console.log(chalk.cyan(`${answer.num1} / ${answer.num2} = ${answer.num1 / answer.num2}`));
+    }
+    else if (answer.operator === "Multiply") {
+        return console.log(chalk.cyan(`${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2}`));
+    }
+};
+const PlayAgain = async () => {
+    let answers = await inquirer.prompt([
+        {
+            name: "Play_Again",
+            type: "confirm",
+            choices: [{
+                    name: "Yes",
+                    type: "number",
+                },
+                {
+                    name: "No",
+                    type: "number",
+                }
+            ],
+            message: "Do You Want to Play Again"
+        }
+    ]);
+    if (answers.Play_Again) {
+        return AskQuestion();
     }
     else {
-        // Something else went wrong
+        return console.log(chalk.greenBright("Thanks for playing"));
     }
-});
+};
+Welcome();
--- index.ts
+++ index.ts
@@ -1,9 +1,132 @@
 import inquirer from 'inquirer';
+import chalk, { Chalk } from 'chalk';
+import chalkAnimation from 'chalk-animation';
 
-let answers = await inquirer.prompt([{
-    name: "age",
-    type: "number",
-    message: "Enter your Age:"}
-]);
 
-console.log("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old.");
+const Welcome = async () => {
+    let rainbow = chalkAnimation.karaoke(chalk.underline("Welcome To Faizan's Calculator"))
+
+
+    await Animation()
+    console.log(`_____________________
+    |  _________________  |
+    | | Faizan          0.| 
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|`)
+    rainbow.stop()
+    AskQuestion()
+}
+
+
+const Animation = () => {
+    return new Promise((res) => {
+        setTimeout(res, 1000)
+    })
+}
+
+const AskQuestion = async () => {
+    inquirer.prompt([{
+        name: "num1",
+        type: "number",
+        message: "Enter 1st Value",
+        validate: function (value) {
+            var pass = !isNaN(value)
+            if (pass) {
+                return true
+            }
+            return console.log(chalk.red('Please enter a valid number'))
+        },
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter 2nd Value"
+    },
+    {
+        name: "operator",
+        type: "list",
+        choices: [{
+            name: "Add",
+            type: "number",
+        },
+        {
+            name: "Substract",
+            type: "number",
+        },
+        {
+            name: "Divide",
+            type: "number",
+        },
+        {
+            name: "Multiply",
+            type: "number",
+        },
+        ],
+        message: "Select Operator"
+    }
+    ]).then((res) => {
+        calulator(res)
+        setTimeout(() => {
+            PlayAgain()
+        }, 1000)
+    }).catch((err) => {
+        console.log(err, 'ererer')
+    })
+
+
+
+}
+
+const calulator = (answer: any) => {
+    if (answer.operator === "Add") {
+        return console.log(chalk.cyan(`${answer.num1} + ${answer.num2} =  ${answer.num1 + answer.num2}`))
+    }
+    else if (answer.operator === "Substract") {
+        return console.log(chalk.cyan(`${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2}`))
+    }
+    else if (answer.operator === "Divide") {
+        return console.log(chalk.cyan(`${answer.num1} / ${answer.num2} = ${answer.num1 / answer.num2}`))
+    }
+    else if (answer.operator === "Multiply") {
+        return console.log(chalk.cyan(`${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2}`))
+    }
+}
+
+const PlayAgain = async () => {
+    let answers = await inquirer.prompt([
+        {
+            name: "Play_Again",
+            type: "confirm",
+            choices: [{
+                name: "Yes",
+                type: "number",
+
+            },
+            {
+                name: "No",
+                type: "number",
+
+            }
+            ],
+            message: "Do You Want to Play Again"
+        }
+    ]);
+    if (answers.Play_Again) {
+        return AskQuestion()
+    } else {
+        return console.log(chalk.greenBright("Thanks for playing"))
+    }
+
+}
+
+Welcome()
+
