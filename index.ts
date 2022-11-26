477769b5059c0463cd03ac609bef0875538dac16
Faizan <faizansohail077@gmail.com>
2022-11-26 15:42:54 +0500

    working on typescript calculator

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt(['hello'
+    
+  ])
+  .then((answers) => {
+    console.log(answers,'answers')
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
